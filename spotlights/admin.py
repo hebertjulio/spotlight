bc646d44b62e26c50b92cbd6a51de7d71e4a9696
Hebert <hebertjulio@users.noreply.github.com>
2020-03-21 14:12:35 -0300

    create spotlights app.

---
+++ spotlights/admin.py
@@ -0,0 +1,73 @@
+from django.contrib import admin
+
+from .models import Site, Section, Editorial, News
+
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'slug', 'url',
+    ]
+    search_fields = [
+        'name', 'slug'
+    ]
+    prepopulated_fields = {
+        'slug': ['name']
+    }
+    readonly_fields = [
+        'created', 'modified'
+    ]
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'slug', 'slots', 'site',
+    ]
+    search_fields = [
+        'name', 'slug'
+    ]
+    prepopulated_fields = {
+        'slug': ['name']
+    }
+    readonly_fields = [
+        'created', 'modified'
+    ]
+    autocomplete_fields = [
+        'site',
+    ]
+
+
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'slug', 'site',
+    ]
+    search_fields = [
+        'name', 'slug'
+    ]
+    prepopulated_fields = {
+        'slug': ['name']
+    }
+    readonly_fields = [
+        'created', 'modified'
+    ]
+    autocomplete_fields = [
+        'site',
+    ]
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = [
+        'headline', 'editorial', 'site',
+    ]
+    search_fields = [
+        'headline', 'blurb', 'url',
+    ]
+    readonly_fields = [
+        'created', 'modified'
+    ]
+    autocomplete_fields = [
+        'site', 'editorial'
+    ]
---
+++ spotlights/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from model_utils.models import TimeStampedModel
+
+
+class Site(TimeStampedModel):
+    name = models.CharField(_('name'), max_length=100)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
+    url = models.URLField(_('url'), unique=True)
+
+    class Meta:
+        verbose_name = 'site'
+        verbose_name_plural = 'sites'
+
+
+class Section(TimeStampedModel):
+    name = models.CharField(_('name'), max_length=100)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
+    slots = models.PositiveSmallIntegerField(_('slots'), default=0)
+    site = models.ForeignKey('Site', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'section'
+        verbose_name_plural = 'sections'
+        unique_together = [
+            ['site', 'slug']
+        ]
+
+
+class Editorial(TimeStampedModel):
+    name = models.CharField(_('name'), max_length=100)
+    slug = models.SlugField(_('slug'), max_length=100)
+    site = models.ForeignKey('Site', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'editorial'
+        verbose_name_plural = 'editorials'
+        unique_together = [
+            ['site', 'slug']
+        ]
+
+
+class News(TimeStampedModel):
+    headline = models.CharField(_('title'), max_length=100)
+    blurb = models.CharField(_('blurb'), max_length=100)
+    editorial = models.ForeignKey('Editorial', on_delete=models.CASCADE)
+    url = models.URLField(_('url'))
+    site = models.ForeignKey('Site', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'news'
+        verbose_name_plural = 'news'
+        unique_together = [
+            ['site', 'url']
+        ]
