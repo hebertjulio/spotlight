cbe507aee7dd655926a848df3be05302fd5ca22e
Hebert <hebertjulio@users.noreply.github.com>
2020-03-22 10:49:46 -0300

    rename spotlight folder to core

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'spotlight.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'spotlight.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
