7cebca9ab75e6fb336a7fc7632e7d0cc0fa712dc
Hebert <hebertjulio@users.noreply.github.com>
2020-03-23 10:32:29 -0300

    hide thumbnail when dont have image

--- spotlights/static/js/custom.js
+++ spotlights/static/js/custom.js
@@ -21,5 +21,11 @@
             }
             selected_section = me.children('option:selected').val();
         });
+
+        let image_field = $('.form-row.field-image>div>p>a');
+        let thumbnail_field = $('.form-row.field-thumbnail');
+        if (thumbnail_field.length > 0 && image_field.length < 1) {
+            thumbnail_field.hide();
+        }
     });
 })(jQuery);
